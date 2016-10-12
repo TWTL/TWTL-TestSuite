12049b21f0292b403be8455d6878a6f990c2a37a
jinyo <solomoj94@hanmail.net>
2016-10-12 23:55:53 +0900

    console 명령을 보내는 세가지 방법에 대해 research

---
+++ JUGOL/Service_Create_Using_SC/Service_Create_Using_SC/Service_Create_Using_SC.cpp
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+#include <stdlib.h>
+int main()
+{
+	//‪C:/Windows/System32/cmd.exe
+	printf("%d\n\n",WinExec("cmd.exe /k jinyo",SW_SHOW));//SW_HIDE//SW_MINIMIZE
+	return 0;
+}
---
+++ JUGOL/Service_Create_Using_SC/Service_Create_Using_SC3/Service_Create_Using_SC3.cpp
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+int main()
+{
+	system("cmd /c ipconfig");
+	return 0;
+}
